446540bec7bb6132d2455c4cd48744a4b3efa64d
coding <lyu63651@gmail.com>
2022-11-07 18:39:15 +1000

    remove #prank

--- main.py
+++ main.py
@@ -11,13 +11,9 @@ import webbrowser
 def help():
     messagebox.askokcancel("Are You Sure Want to continue?", "Want to continue?")
     webbrowser.open('https://https://www.naturalreaders.com/online/support/help')
+    
+    
 
-
-def buy():
-    #It a joke
-    messagebox.askokcancel("Are You Sure Want To Buy?", "Are You Sure Want To Buy?")
-    messagebox.showerror('Error','Error code 0x100029')
-    messagebox.askretrycancel("Error", "Try again?")
 
 
 
@@ -34,24 +30,15 @@ def randomoutput():
     randomtext = tk.Label(text=output)
     randomtext.pack()
     print("Your Random Number is %d" % output)
-    #prank
-    window.destroy()
-    messagebox.showerror('Error','Error code 0x10000964')
-    messagebox.askretrycancel("Error", "Try again?")
-    messagebox.askretrycancel("Python Crash", "Python Crash,Try again?")
-    messagebox.showerror('Error','Error code 0x08400964')
-
 
 
 window = tk.Tk()
 window.geometry("500x200")
-window.title('random generator')
+window.title('random number generator')
 randombutton=tk.Button(text='Generate',command=randomoutput)
 randombutton.pack()
 close = tk.Button(text='Close',command=close)
 close.pack()
-buy = tk.Button(text='Buy This software',command=buy)
-buy.pack()
 help = tk.Button(text='Help',command=help)
 help.pack()
 window.mainloop()
